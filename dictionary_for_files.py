1ff2af172405207b6133fbff360d88226216a785
MariSys <77438847+MariSys@users.noreply.github.com>
2021-02-16 11:29:25 +0100

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -1,12 +1,12 @@
 storage = {
-'dna extraction xom':'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
-'dna extraction mag':'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing'
+'dna extraction xom' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
+'dna extraction mag' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
+'rna extraction' : 'link',
+'reverse transcription' : 'link1',
+'usual PCR' : 'link2',
+'rt-PCR with zond' : 'link3',
+'rt-PCR with SB' : 'link4'
 }
+# пока так, ждем заполнения файлов со ссылками на гугл диск или привлечением самого гугл API, прошу сохранить названия слов в ключах, потому что клавиатура ссылается на них 
 
 
-''' сам костыль
-def protocol_responce(message):
-    for i in storage.keys():
-        if message.text.lower() == i:
-            bot.send_message(message.from_user.id, storage[i])
-'''
