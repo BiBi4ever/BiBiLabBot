bf84529bee0a4b53dbb3d6ed394122d9bb89ea1d
MariSys <77438847+MariSys@users.noreply.github.com>
2021-02-16 11:37:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,18 @@ token = os.environ.get('token')
 #картинка
 url = "https://sun9-40.userapi.com/impg/mG_WTIdgArErQb4YbU7CEIDz873dDvJoH0VW-w/arHUSXBmA5Y.jpg?size=527x505&quality=96&proxy=1&sign=3103cde7044a879a6d8e76a5b8ab2d62&type=album"
 
+#словарь пока что здесь
+storage = {
+'dna extraction xom' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
+'dna extraction mag' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
+'rna extraction' : 'link',
+'reverse transcription' : 'link1',
+'usual PCR' : 'link2',
+'rt-PCR with zond' : 'link3',
+'rt-PCR with SB' : 'link4'
+}
+
 bot = telebot.TeleBot(token)
-keyboard1 = telebot.types.ReplyKeyboardMarkup(False,True)
-keyboard1.row('Рассчитать концентрацию', 'Сохранить протокол', 'Предобратотать данные','Показать результаты')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -18,17 +27,66 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /help', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /help. С моей помощью ты можешь увидеть протоколы по '
+                                      'работе с нуклеиновыми кислотами (выделение ДНК и РНК, обратная транскрипция) и '
+                                      'протоколы ПЦР(обычная, реал-тайм с зондом или реал-тайм с красителем SYBR). Для начала напиши /protocols. ' 
+                     'Если хочешь вводить запрос самостоятельно - выбирай поиск по ключу, если хочешь увидеть все возможные протоколы -выбирай поиск с кнопками')
+)
+# Команда для работы с протоколами, если пользователь выбирает поиск по ключу (вызов key), он вводит слова для протокола сам, если выбирает кнопку ( вызов button) - тыкает на варианты
+@bot.message_handler(commands=['protocols'])
+def exchange_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Поиск по ключу', callback_data='key'),
+        telebot.types.InlineKeyboardButton('Поиск с кнопками',callback_data='button'))
+    bot.send_message(message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard)
 
+    @bot.callback_query_handler(func=lambda call: call.data in ['key', 'button'] )
+    def query_handler(call1):
+        if call1.data == 'key':
+            bot.send_message(message.chat.id, 'Напишите нужный ключ. Учитываются варианты') #(Можете перечислить допустимые слова)
+            #МЕСТО ДЛЯ КОДА ПОИСКА ПО КЛЮЧУ, ДОЛЖНО НАЧИНАТЬСЯ С ВВОДА КЛЮЧА
+         
+         
+         
+        elif call1.data == 'button':
+            keyboard1 = telebot.types.InlineKeyboardMarkup()
+            keyboard1.row(
+                telebot.types.InlineKeyboardButton('Работа с нуклеиновыми кислотами', callback_data='acid'),
+                telebot.types.InlineKeyboardButton('Работа с ПЦР', callback_data='PCR'))
+            bot.send_message(message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard1)
+
+            @bot.callback_query_handler(func=lambda call: call.data in ['acid', 'PCR'] )
+            def query_handler2(call2):
+                if call2.data == 'acid':
+                    keyboard2 = telebot.types.InlineKeyboardMarkup(row_width=2)
+                    keyboard2.add(
+                    telebot.types.InlineKeyboardButton('Выделение ДНК по Хомчински', callback_data='xom'),
+                    telebot.types.InlineKeyboardButton('Выделение ДНК на магнитах', callback_data='mag'),
+                    telebot.types.InlineKeyboardButton('Выделение РНК ', callback_data='rna'),
+                    telebot.types.InlineKeyboardButton('Обратная транскрипция', callback_data='reverse'))
+                    bot.send_message(message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard2)
+
+                elif call2.data == 'PCR':
+                    keyboard2 = telebot.types.InlineKeyboardMarkup(row_width=2)
+                    keyboard2.add(
+                telebot.types.InlineKeyboardButton('Обычная ПЦР', callback_data='usual'),
+                telebot.types.InlineKeyboardButton('Реал тайм ПЦР с зондами', callback_data='zond'),
+                telebot.types.InlineKeyboardButton('Реал тайм ПЦР на sybr green', callback_data='SB'))
+                    bot.send_message(message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard2)
+
+                @bot.callback_query_handler(func=lambda call: call.data in ['xom','mag','rna','reverse','usual','zond','SB'])
+                def query_handler3(call3):
+                    for i in storage.keys():
+                        if call3.data in i:
+                            bot.send_message(message.from_user.id, storage[i])
+#Приветствие
 @bot.message_handler(content_types=['text'])
 def send_first_message(message):
     greet = ['hello','hi','привет']
-    task = ['рассчитать концентрацию', 'сохранить протокол', 'предобратотать данные','показать результаты']
     if any(greetings in message.text.lower() for greetings in greet):
         bot.send_message(message.from_user.id, 'Hello you! Со мной ты можешь быстро рассчитать необходимые концентрации, предобработать данные, оценить их качество и сделать многое другое! Напиши \'/help\', чтобы увидеть все возможные функции.')
-    elif any(word in message.text.lower() for word in task):
-        bot.send_message(message.from_user.id, 'упс, пока что функция в разработке')
-#бот кидает мемосную картиночку
+#бот кидает мемосную картиночку, если пользователь вводит неправильный запрос
     else:
         img = Image.open(urlopen(url))
         bot.send_photo(message.chat.id, img)
