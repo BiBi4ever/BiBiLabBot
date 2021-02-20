bca02124b4273154615579227641a0a538efb63b
EKarpeeva <77439171+EKarpeeva@users.noreply.github.com>
2021-02-20 10:39:35 +0200

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -1,11 +1,12 @@
 storage = {
 'dna extraction xom' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
 'dna extraction mag' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
-'rna extraction' : 'link',
-'reverse transcription' : 'link1',
+'rna extraction' : 'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
+'reverse transcription' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
 'usual PCR' : 'link2',
 'rt-PCR with zond' : 'link3',
-'rt-PCR with SB' : 'link4'
+'rt-PCR with SB' : 'link4',
+'matohondria extraction' : 'https://drive.google.com/file/d/1qdCp3c_XPhX-r2aCq27c5JbfsbAvLIwg/view'
 }
 # пока так, ждем заполнения файлов со ссылками на гугл диск или привлечением самого гугл API, прошу сохранить названия слов в ключах, потому что клавиатура ссылается на них 
 
