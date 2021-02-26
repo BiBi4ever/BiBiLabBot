407b3cb04be377794534402163cd06e46d7122f4
MariSys <77438847+MariSys@users.noreply.github.com>
2021-02-26 17:19:38 +0100

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -5,8 +5,7 @@ storage = {
 'reverse transcription' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
 'usual PCR' : 'link2',
 'rt-PCR with zond' : 'link3',
-'rt-PCR with SB' : 'link4',
-'matohondria extraction' : 'https://drive.google.com/file/d/1qdCp3c_XPhX-r2aCq27c5JbfsbAvLIwg/view'
+'rt-PCR with SB' : 'link4'
 }
 # пока так, ждем заполнения файлов со ссылками на гугл диск или привлечением самого гугл API, прошу сохранить названия слов в ключах, потому что клавиатура ссылается на них 
 
