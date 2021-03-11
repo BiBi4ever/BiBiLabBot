1ab4217ae28549de97f596bcdf3f3fe0c71ab88d
Катерина <77439171+EKarpeeva@users.noreply.github.com>
2021-03-11 14:55:53 +0200

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -1,14 +1,3 @@
-storage = {
-'dna extraction xom' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
-'dna extraction mag' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
-'rna extraction' : 'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
-'reverse transcription' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
-'usual PCR' : 'link2',
-'rt-PCR with zond' : 'link3',
-'rt-PCR with SB' : 'link4'
-}
-# пока так, ждем заполнения файлов со ссылками на гугл диск или привлечением самого гугл API, прошу сохранить названия слов в ключах, потому что клавиатура ссылается на них 
-
 # словарь для поиска по ключевым словам (см функцию внизу в коде)
 storageKey = {
     'выделение днк хомченко по-хомченко dna' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
