0b7db9e4d92b90be7bfdc6b01fc22c0d651ce176
Катерина <77439171+EKarpeeva@users.noreply.github.com>
2021-03-11 14:47:38 +0200

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -9,4 +9,13 @@ storage = {
 }
 # пока так, ждем заполнения файлов со ссылками на гугл диск или привлечением самого гугл API, прошу сохранить названия слов в ключах, потому что клавиатура ссылается на них 
 
-
+# словарь для поиска по ключевым словам (см функцию внизу в коде)
+storageKey = {
+    'выделение днк хомченко по-хомченко dna' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
+'выделение днк магнитные частицы магниты dna' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
+'выделение рнк rna' : 'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
+'обратная транскрипция reverse transcription' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
+'обычная пцр usual PCR' : 'link2',
+'реал тайм real time  пцр с зондом rt-PCR with zond' : 'link3',
+'реал тайм real time пцр sybr green rt-PCR with SB' : 'link4'
+}
