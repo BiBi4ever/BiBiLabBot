302e8576acd3331d064999eaa18c670f37b839b6
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-11 23:39:00 +0200

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -1,10 +1,10 @@
 # словарь для поиска по ключевым словам (см функцию внизу в коде)
 storageKey = {
-    'выделение днк хомченко по-хомченко dna' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
+    'выделение днк хомченски по-хомченски dna' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
 'выделение днк магнитные частицы магниты dna' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
 'выделение рнк rna' : 'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
-'обратная транскрипция reverse transcription' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
+'обратная транскрипция reverse transcription MMLV cDNA кДНК' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
 'обычная пцр usual PCR' : 'link2',
 'реал тайм real time пцр с зондом rt-PCR with zond' : 'link3',
-'реал тайм real time пцр sybr green rt-PCR with SB' : 'link4'
+'реал тайм real time пцр sybr green rt-PCR with SG' : 'link4'
 }
