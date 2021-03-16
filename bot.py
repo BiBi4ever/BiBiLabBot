227e5c0fb63ef3f52e04751c85ecda8c94adf289
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-16 20:24:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def exchange_command(message):
     def query_handler(call1):
         if call1.data == 'key':
             # поиск по ключу, см. функцию ниже:
-            bot.send_messege(chat_id=call1.message.chat.id, text='Введи ключевое слово')
+            bot.send_messege(call1.message.chat.id, text='Введи ключевое слово')
             bot.register_next_step_handler(send,keys)
          
         elif call1.data == 'button':
