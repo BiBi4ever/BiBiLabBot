405488b57f66bb270c45dd8c88f20ea7774f5871
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-16 21:32:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,6 @@ def exchange_command(message):
                 telebot.types.InlineKeyboardButton('Работа с нуклеиновыми кислотами', callback_data='acid'),
                 telebot.types.InlineKeyboardButton('Работа с ПЦР', callback_data='PCR'))
             bot.send_message(call1.message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard1)
-            bot.edit_message_reply_markup(call1.message.chat.id, call.message.message_id)
          
     @bot.callback_query_handler(func=lambda call2: call2.data in ['acid', 'PCR'] )
     def query_handler2(call2):
@@ -74,6 +73,7 @@ def keys(message):
     if len(found_links) > 0:
         bot.send_message(message.from_user.id, "\n\n".join(found_links) + '\n\n\U0001F50E Чтобы начать новый поиск, нажми /protocols')
     else:
-        bot.send_message(message.from_user.id,
-                                 'Совпадений не найдено. Попробуй ввести другое слово, например: ДНК.\nНажми /protocols, чтобы начать поиск')      
+        send_me = bot.send_message(message.from_user.id,
+                                 'Совпадений не найдено. Попробуй ввести другое слово, например: ДНК')
+        bot.register_next_step_handler(send_me, keys)
 bot.polling(True)
