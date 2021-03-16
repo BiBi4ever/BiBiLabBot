dede815bd1b51a4fec964d5631d64ca7cf070ff6
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-16 20:30:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ def exchange_command(message):
                 telebot.types.InlineKeyboardButton('Реал тайм ПЦР на sybr green', url='https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/11.png'))
             bot.send_message(call2.message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard2) 
                   
-   def keys(message):
+def keys(message):
     list = [*storageKey]
     found_links = []
     for i in list:
@@ -76,5 +76,5 @@ def exchange_command(message):
         bot.send_message(message.from_user.id, "\n\n".join(found_links) + '\n\n\U0001F50E Чтобы начать новый поиск, нажми /protocols')
     else:
         bot.send_message(message.from_user.id,
-                                 'Совпадений не найдено. Попробуй ввести другое слово, например: ДНК.\nНажми /protocols, чтобы начать поиск')   
+                                 'Совпадений не найдено. Попробуй ввести другое слово, например: ДНК.\nНажми /protocols, чтобы начать поиск')      
 bot.polling(True)
