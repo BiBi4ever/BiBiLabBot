e0b55a00fb7d029db39a03668f79fc591a4446a5
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-17 00:39:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,35 +28,32 @@ def exchange_command(message):
             bot.register_next_step_handler(send,keys)
          
         elif call1.data == 'button':
-            keyboard1 = telebot.types.InlineKeyboardMarkup()
-            keyboard1.row(
+            keyboard1 = telebot.types.InlineKeyboardMarkup().row(
                 telebot.types.InlineKeyboardButton('Работа с нуклеиновыми кислотами', callback_data='acid'),
                 telebot.types.InlineKeyboardButton('Работа с ПЦР', callback_data='PCR'))
-            bot.send_message(call1.message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard1)
+            bot.send_message(call1.message.chat.id, ' ', reply_markup=keyboard1)
             
     @bot.callback_query_handler(func=lambda call2: call2.data in ['acid', 'PCR'] )
     def query_handler2(call2):
         if call2.data == 'acid':
-            keyboard2 = telebot.types.InlineKeyboardMarkup(row_width=2)
-            keyboard2.add(
+            keyboard2 = telebot.types.InlineKeyboardMarkup(row_width=2).add(
                     telebot.types.InlineKeyboardButton('Выделение ДНК по Хомчински', url='https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing'),
                     telebot.types.InlineKeyboardButton('Выделение ДНК на магнитах', url='https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing'),
                     telebot.types.InlineKeyboardButton('Выделение РНК ', url='https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view'),
                     telebot.types.InlineKeyboardButton('Обратная транскрипция', url='https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view'))
-            bot.send_message(call2.message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard2)
+            bot.send_message(call2.message.chat.id, ' ', reply_markup=keyboard2)
                   
         elif call2.data == 'PCR':
-            keyboard2 = telebot.types.InlineKeyboardMarkup(row_width=2)
-            keyboard2.add(
+            keyboard2 = telebot.types.InlineKeyboardMarkup(row_width=2).add(
                 telebot.types.InlineKeyboardButton('Обычная ПЦР', url='https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/1.png'),
                 telebot.types.InlineKeyboardButton('Реал тайм ПЦР с зондами', url='https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/8.png'),
                 telebot.types.InlineKeyboardButton('Реал тайм ПЦР на sybr green', url='https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/11.png'))
-            bot.send_message(call2.message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard2) 
+            bot.send_message(call2.message.chat.id, ' ', reply_markup=keyboard2) 
                  
 def keys(message):
-    list = [storageKey]
-    found_links = []
-    for i in list:
+    dickt = storageKey 
+    found_links=[]
+    for i in dickt:
         if message.text.lower() in i:
             found_links.append(storageKey[i])
     if len(found_links) <= 0:
@@ -65,5 +62,4 @@ def keys(message):
         bot.register_next_step_handler(send_me, keys)
         return
     send_me = bot.send_message(message.from_user.id, "\n\n".join(found_links) + '\n\n\U0001F50E Чтобы начать новый поиск, нажми /protocols')
-    
 bot.polling(True)
