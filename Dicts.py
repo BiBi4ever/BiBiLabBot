b84ca07b73db6265a5d7e3cbc0009142499df33f
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-18 01:19:01 +0200

    Update Dicts.py

--- Dicts.py
+++ Dicts.py
@@ -11,13 +11,13 @@ callback_query_handler2={'Назад':'back2',
                         'Назад ':'back3'}
 
 callback_data_keyboard_Acid={'Выделение ДНК по Хомчински':'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
-                        'Выделение ДНК на магнитах':'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
-                        'Выделение РНК':'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
-                        'Обратная транскрипция':'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view'}
+                             'Выделение ДНК на магнитах':'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
+                             'Выделение РНК ':'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
+                             'Обратная транскрипция':'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view'}
 
 callback_data_keyboard_PCR={'Обычная ПЦР':'https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/1.png',
-                        'Реал тайм ПЦР с зондами': 'https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/8.png',
-                        'Реал тайм ПЦР на sybr green':'https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/11.png'}
+                            'Реал тайм ПЦР с зондами':'https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/8.png',
+                            'Реал тайм ПЦР на sybr green':'https://s.tcdn.co/ec5/c1b/ec5c1b75-12ea-45bd-aa7b-33491089b8e5/11.png'}
 
 keyboard = telebot.types.InlineKeyboardMarkup().add(
         telebot.types.InlineKeyboardButton([key for key in callback_query_handler][0],callback_data=[values for values in callback_query_handler.values()][0]),
@@ -36,8 +36,8 @@ keyboard2 = telebot.types.InlineKeyboardMarkup().add(
             telebot.types.InlineKeyboardButton([key for key in callback_query_handler2][0], callback_data=[values for values in callback_query_handler2.values()][0]))
 
 keyboard3 = telebot.types.InlineKeyboardMarkup().add(
-            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][0],callback_data=[values for values in callback_data_keyboard_PCR.values()][0]),
-            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][1],callback_data=[values for values in callback_data_keyboard_PCR.values()][1]),
-            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][2],callback_data=[values for values in callback_data_keyboard_PCR.values()][2]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][0], url=[values for values in callback_data_keyboard_PCR.values()][0]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][1], url=[values for values in callback_data_keyboard_PCR.values()][1]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][2], url=[values for values in callback_data_keyboard_PCR.values()][2]),
             telebot.types.InlineKeyboardButton([key for key in callback_query_handler2][1],callback_data=[values for values in callback_query_handler2.values()][1]))
 
