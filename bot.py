6916ea4653348ae66cc88e6eeca9c46dfed26650
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-18 01:33:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,40 +13,65 @@ url = "https://sun9-40.userapi.com/impg/mG_WTIdgArErQb4YbU7CEIDz873dDvJoH0VW-w/a
 
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+         bot.reply_to(message, f'Bonjour, {message.from_user.first_name}!\U0001F44B\nЯ бот, облегчающий работу в лаборатории. \n\nУ меня есть база протоколов, которые могут пригодиться в твоих исследованиях. \n\nЧтобы начать поиск протокола, нажми /protocols.\n\nЧтобы увидеть список доступных действий, нажми /help.')
+                               
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id, ' С моей помощью ты можешь увидеть протоколы для '
+                                      'работы с нуклеиновыми кислотами, обратной транскрипции и '
+                                      'ПЦР.\n\n' 
+                     'Чтобы найти нужный протокол, нажми /protocols. Выбирай поиск с кнопками или воспользуйся поиском по ключу и введи ключевое слово.')
+
+#Ответ на приветствие
+@bot.message_handler(content_types=['text'])
+def send_first_message(message):
+    greet = ['hello','hi','привет', 'здравствуй']
+    if any(greetings in message.text.lower() for greetings in greet):
+        bot.send_message(message.from_user.id, 'Рад тебя видеть! Я скучал')
+
+#бот кидает мемосную картиночку, если пользователь вводит неправильный запрос
+    else:
+        img = Image.open(urlopen(url))
+        bot.send_photo(message.chat.id, img)
+        img.close()
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит. Если тебе нужна помощь, нажми /help')
+    
 #Ключ или кнопки
 @bot.message_handler(commands=['protocols'])
 def callback_handler(message):    
-    bot.send_message(message.chat.id, 'Выберите нужный вариант:', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Можешь выбрать нужный вариант', reply_markup=keyboard)
     
     @bot.callback_query_handler(func=lambda call1: call1.data in [value for value in callback_query_handler.values()])
     def query_handler(call1):
         if call1.data == 'key':
-            send = bot.edit_message_text(chat_id=call1.message.chat.id, message_id=call1.message.message_id, text='Введите слово')
+            send = bot.edit_message_text(chat_id=call1.message.chat.id, message_id=call1.message.message_id, text='Введи слово')
             bot.register_next_step_handler(send,keys)
             #Переписывает предыдущее сообщение, кнопки пропадают, код переходит на функцию поиска по ключам,которая ниже
         elif call1.data == 'button':
-            bot.edit_message_text(chat_id=call1.message.chat.id, message_id=call1.message.message_id, text='Выберите нужный вариант:', reply_markup=keyboard1)
+            bot.edit_message_text(chat_id=call1.message.chat.id, message_id=call1.message.message_id, text='Можешь выбрать нужный вариант', reply_markup=keyboard1)
             #Переписывает предыдущее сообщение и добавляет новую клавиатуру для выбора дальше по кнопкам
             
     @bot.callback_query_handler(func=lambda call2: call2.data in [value for value in callback_query_handler1.values()])
     def query_handler1(call2):
         if call2.data == 'acid':
-            bot.edit_message_text(chat_id=call2.message.chat.id, message_id=call2.message.message_id, text='Выберите нужный вариант', reply_markup=keyboard2)
+            bot.edit_message_text(chat_id=call2.message.chat.id, message_id=call2.message.message_id, text='Можешь выбрать нужный вариант', reply_markup=keyboard2)
             #Переписывает предыдущее сообщение и добавляет новую клавиатуру для выбора дальше по кнопкам
                   
         elif call2.data == 'PCR':
-            bot.edit_message_text(chat_id=call2.message.chat.id, message_id=call2.message.message_id, text='Выберите нужный вариант', reply_markup=keyboard3) 
+            bot.edit_message_text(chat_id=call2.message.chat.id, message_id=call2.message.message_id, text='Можешь выбрать нужный вариант', reply_markup=keyboard3) 
             #Переписывает предыдущее сообщение и добавляет новую клавиатуру для выбора дальше по кнопкам 
     
         elif call2.data == 'back':
-            bot.edit_message_text(chat_id=call2.message.chat.id, message_id=call2.message.message_id, text='Выберите нужный вариант:', reply_markup=keyboard)
+            bot.edit_message_text(chat_id=call2.message.chat.id, message_id=call2.message.message_id, text='Можешь выбрать нужный вариант', reply_markup=keyboard)
         
     @bot.callback_query_handler(func=lambda call3: call3.data in [value for value in callback_query_handler2.values()] )
     def query_handler2(call3):
         if call3.data == 'back1':
-            bot.edit_message_text(chat_id=call3.message.chat.id, message_id=call3.message.message_id, text='Выберите нужный вариант:', reply_markup=keyboard1)
+            bot.edit_message_text(chat_id=call3.message.chat.id, message_id=call3.message.message_id, text='Можешь выбрать нужный вариант', reply_markup=keyboard1)
         elif call3.data == 'back2':
-            bot.edit_message_text(chat_id=call3.message.chat.id, message_id=call3.message.message_id, text='Выберите нужный вариант:', reply_markup=keyboard1)
+            bot.edit_message_text(chat_id=call3.message.chat.id, message_id=call3.message.message_id, text='Можешь выбрать нужный вариант', reply_markup=keyboard1)
                 
                 
 def keys(message):
@@ -57,9 +82,9 @@ def keys(message):
             found_links.append(storageKey[i])
     if len(found_links) <= 0:
         send_me = bot.send_message(message.from_user.id,
-                                 'Совпадений не найдено. Попробуйте ввести другое слово, например: ДНК \n Или нажмите /protocols, чтобы начать поиск')
+                                 'Совпадений не найдено. Попробуй ввести другое слово, например: ДНК \n Или нажми /protocols, чтобы начать поиск')
         bot.register_next_step_handler(send_me, keys)
         return
-    send_me = bot.send_message(message.from_user.id, "\n\n".join(found_links) + '\n\n\U0001F50E Чтобы начать новый поиск, нажмите /protocols')
+    send_me = bot.send_message(message.from_user.id, "\n\n".join(found_links) + '\n\n Чтобы начать новый поиск, нажми /protocols')
     
 bot.polling(True)
