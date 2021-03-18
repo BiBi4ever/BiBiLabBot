518cedad60c97cd3472cb4feea98b1c996fa24d4
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-19 00:48:00 +0200

    Update Dicts.py

--- Dicts.py
+++ Dicts.py
@@ -25,7 +25,7 @@ keyboard_list_of_bottons3 = []
 
 for i in callback_query_handler:
     keyboard_list_of_bottons.append(telebot.types.InlineKeyboardButton(i,callback_data=callback_query_handler[i]))
-    keyboard_for_buttons = telebot.types.InlineKeyboardMarkup().add(keyboard_list_of_bottons)
+keyboard_for_buttons = telebot.types.InlineKeyboardMarkup().add(keyboard_list_of_bottons)
 
 keyboard_for_buttons1 = telebot.types.InlineKeyboardMarkup().add(
             telebot.types.InlineKeyboardButton([key for key in callback_query_handler1][0],callback_data=[values for values in callback_query_handler1.values()][0]),
