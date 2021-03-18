a4849056c3686705f87ae084a1fb271f92cf463b
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-19 00:21:07 +0200

    Update Dicts.py

--- Dicts.py
+++ Dicts.py
@@ -22,20 +22,21 @@ for i in callback_query_handler:
     button = telebot.types.InlineKeyboardButton([i],callback_data=callback_query_handler[i])
     keyboard_for_buttons = telebot.types.InlineKeyboardMarkup().add(button)
 
-for i in callback_query_handler1:
-    button1 = telebot.types.InlineKeyboardButton([i],callback_data=callback_query_handler1[i])
-    keyboard_for_buttons1 = telebot.types.InlineKeyboardMarkup().add(button1)
+keyboard_for_buttons1 = telebot.types.InlineKeyboardMarkup().add(
+            telebot.types.InlineKeyboardButton([key for key in callback_query_handler1][0],callback_data=[values for values in callback_query_handler1.values()][0]),
+            telebot.types.InlineKeyboardButton([key for key in callback_query_handler1][1],callback_data=[values for values in callback_query_handler1.values()][1]),
+            telebot.types.InlineKeyboardButton([key for key in callback_query_handler1][2],callback_data=[values for values in callback_query_handler1.values()][2]))
 
-for i in callback_data_keyboard_Acid:
-    button2 = telebot.types.InlineKeyboardButton([i], url=callback_data_keyboard_Acid[i])
-    keyboard_for_buttons2 = telebot.types.InlineKeyboardMarkup().add(button2)
+keyboard_for_buttons2 = telebot.types.InlineKeyboardMarkup().add(
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][0], url=[values for values in callback_data_keyboard_Acid.values()][0]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][1], url=[values for values in callback_data_keyboard_Acid.values()][1]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][2], url=[values for values in callback_data_keyboard_Acid.values()][2]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][3], url=[values for values in callback_data_keyboard_Acid.values()][3]),
+            telebot.types.InlineKeyboardButton([key for key in callback_query_handler2][0], callback_data=[values for values in callback_query_handler2.values()][0]))
 
-for i in callback_data_keyboard_PCR:
-    button3 = telebot.types.InlineKeyboardButton([i], url=callback_data_keyboard_PCR[i])
-    keyboard_for_buttons3 = telebot.types.InlineKeyboardMarkup().add(button3)
+keyboard_for_buttons3 = telebot.types.InlineKeyboardMarkup().add(
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][0],callback_data=[values for values in callback_data_keyboard_PCR.values()][0]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][1],callback_data=[values for values in callback_data_keyboard_PCR.values()][1]),
+            telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][2],callback_data=[values for values in callback_data_keyboard_PCR.values()][2]),
+            telebot.types.InlineKeyboardButton([key for key in callback_query_handler2][1],callback_data=[values for values in callback_query_handler2.values()][1]))
 
-List_keyboards_with_url = [keyboard_for_buttons2, keyboard_for_buttons3] 
-    
-for i in range(len(List_keyboards_with_url)):
-    button_back = telebot.types.InlineKeyboardButton([key for key in callback_query_handler2][0], callback_data=[values for values in callback_query_handler2.values()][0])
-    i = telebot.types.InlineKeyboardMarkup().add(button_back)
