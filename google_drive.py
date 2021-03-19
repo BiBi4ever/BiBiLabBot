ba150565b378881b5b2953f5a1ddaebce033499d
Valeriya Mazur <77438746+valeriyamazur@users.noreply.github.com>
2021-03-19 23:59:29 +0200

    Update google_drive.py

--- google_drive.py
+++ google_drive.py
@@ -1,3 +1,6 @@
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+
 def authorization (ID):
   SCOPES = ['https://www.googleapis.com/auth/drive']
   credentials = ServiceAccountCredentials.from_json_keyfile_dict(
