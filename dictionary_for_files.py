85d2a1db5e73d79f857946ca35964fd9f37be788
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-03-19 12:15:41 +0200

    Update dictionary_for_files.py

--- dictionary_for_files.py
+++ dictionary_for_files.py
@@ -1,5 +1,8 @@
 # словарь для поиска по ключевым словам (см функцию внизу в коде)
-storageKey = {'выделение днк хомченски по-хомченски dna' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
+storageKey = {'/protocols':'',
+              '/help':'',
+              '/start':'',
+              'выделение днк хомченски по-хомченски dna' : 'https://drive.google.com/file/d/1DmogZzc5-vEgDxxqiCB4sC3wHOb9KYHc/view?usp=sharing',
 'выделение днк магнитные частицы магниты dna' : 'https://drive.google.com/file/d/1C_TYw363bHUPfdFXumlmeqA1TEDP3YEd/view?usp=sharing',
 'выделение рнк rna' : 'https://drive.google.com/file/d/1mzLZRFX3hDsQpm18QD_op8mg89E29Z-P/view',
 'обратная транскрипция reverse transcription MMLV cDNA кДНК' : 'https://drive.google.com/file/d/1uZr7I87Ow6VqzTTBqg_0OzuriqUm-Ip-/view',
