fbbd2bdb9973d8533d63c16dd773c149405ca3f8
Valeriya Mazur <77438746+valeriyamazur@users.noreply.github.com>
2021-03-19 23:49:56 +0200

    Create google_drive.py

---
+++ google_drive.py
@@ -0,0 +1,6 @@
+def authorization (ID):
+  SCOPES = ['https://www.googleapis.com/auth/drive']
+  credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+       ID, scopes=SCOPES)
+  service = build('drive', 'v3', credentials=credentials)
+  return service
