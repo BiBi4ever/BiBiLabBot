f5ff170e9d28d917b2aa0d3b17f1fbf9b608cae4
Valeriya Mazur <77438746+valeriyamazur@users.noreply.github.com>
2021-04-06 12:14:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,8 +95,8 @@ def keys(message):
 
          results = service.files().list(fields="files(name, id)", q =("name contains '%s'" % message.text.lower()) ).execute()
          
-         if  results.get('files', []):
-                  for file in results.get('files', []):
+         if  results.get('files'):
+                  for file in results.get('files'):
                            filename = file.get('name')
                            request = service.files().get_media(fileId=file.get('id'))
                            fh = io.FileIO(filename, 'wb')
