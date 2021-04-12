0c0f707c3f4aef1a1753376b9c45db7d79a03103
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-04-13 01:11:37 +0200

    Update Dicts.py

--- Dicts.py
+++ Dicts.py
@@ -9,14 +9,14 @@ callback_query_handler1={'Работа с НК':'acid',
                          
 callback_query_handler2={'Назад':'back1'}
 
-callback_data_keyboard_Acid={'Выделение ДНК \nпо Хомчински':'DNA_KCHOMCHINSKY',
-                        'Выделение ДНК \nна магнитах':'DNA_MAGNET',
+callback_data_keyboard_Acid={'Выделение ДНК по Хомчински':'DNA_KCHOMCHINSKY',
+                        'Выделение ДНК на магнитах':'DNA_MAGNET',
                         'Выделение РНК':'RNA',
-                        'Обратная \nтранскрипция':'REVERSE_TRANSCRIPTION'}
+                        'Обратная транскрипция':'REVERSE_TRANSCRIPTION'}
 
 callback_data_keyboard_PCR={'Обычная ПЦР':'PCR_REARE',
-                        'Реал тайм \n ПЦР зонд': 'PCR_WITH_ZOND',
-                        'Реал тайм \n ПЦР sybr':'PCR_SYBRE'}
+                        'Real time ПЦР зонд': 'PCR_WITH_ZOND',
+                        'Real time ПЦР sybr':'PCR_SYBRE'}
 
 keyboard_for_buttons = telebot.types.InlineKeyboardMarkup().add(
         telebot.types.InlineKeyboardButton([key for key in callback_query_handler][0],callback_data=[values for values in callback_query_handler.values()][0]),
@@ -27,14 +27,14 @@ keyboard_for_buttons1 = telebot.types.InlineKeyboardMarkup(row_width=2).add(
             telebot.types.InlineKeyboardButton([key for key in callback_query_handler1][1],callback_data=[values for values in callback_query_handler1.values()][1]),
             telebot.types.InlineKeyboardButton([key for key in callback_query_handler1][2],callback_data=[values for values in callback_query_handler1.values()][2]))
 
-keyboard_for_buttons2 = telebot.types.InlineKeyboardMarkup(row_width=2).add(
+keyboard_for_buttons2 = telebot.types.InlineKeyboardMarkup(row_width=1).add(
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][0], callback_data=[values for values in callback_data_keyboard_Acid.values()][0]),
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][1], callback_data=[values for values in callback_data_keyboard_Acid.values()][1]),
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][2], callback_data=[values for values in callback_data_keyboard_Acid.values()][2]),
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_Acid][3], callback_data=[values for values in callback_data_keyboard_Acid.values()][3]),
             telebot.types.InlineKeyboardButton([key for key in callback_query_handler2][0], callback_data=[values for values in callback_query_handler2.values()][0]))
 
-keyboard_for_buttons3 = telebot.types.InlineKeyboardMarkup(row_width=2).add(
+keyboard_for_buttons3 = telebot.types.InlineKeyboardMarkup(row_width=1).add(
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][0],callback_data=[values for values in callback_data_keyboard_PCR.values()][0]),
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][1], callback_data=[values for values in callback_data_keyboard_PCR.values()][1]),
             telebot.types.InlineKeyboardButton([key for key in callback_data_keyboard_PCR][2], callback_data=[values for values in callback_data_keyboard_PCR.values()][2]),
