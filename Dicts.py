f5eed54b4608ee54468873aa3e2366c721f68302
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-04-13 00:55:28 +0200

    Update Dicts.py
    кнопкии более понятные на телефоне

--- Dicts.py
+++ Dicts.py
@@ -3,20 +3,20 @@ import telebot
 callback_query_handler={'Поиск по ключу':'key',
                         'Поиск с кнопками':'button'}
 
-callback_query_handler1={'Работа с нуклеиновыми кислотами':'acid',
-                        'Работа с ПЦР':'PCR',
+callback_query_handler1={'Работа с НК':'acid',
+                        'ПЦР':'PCR',
                         'Назад':'back'}
                          
 callback_query_handler2={'Назад':'back1'}
 
-callback_data_keyboard_Acid={'Выделение ДНК по Хомчински':'DNA_KCHOMCHINSKY',
-                        'Выделение ДНК на магнитах':'DNA_MAGNET',
+callback_data_keyboard_Acid={'Выделение ДНК \nпо Хомчински':'DNA_KCHOMCHINSKY',
+                        'Выделение ДНК \nна магнитах':'DNA_MAGNET',
                         'Выделение РНК':'RNA',
-                        'Обратная транскрипция':'REVERSE_TRANSCRIPTION'}
+                        'Обратная \nтранскрипция':'REVERSE_TRANSCRIPTION'}
 
 callback_data_keyboard_PCR={'Обычная ПЦР':'PCR_REARE',
-                        'Реал тайм ПЦР с зондами': 'PCR_WITH_ZOND',
-                        'Реал тайм ПЦР на sybr green':'PCR_SYBRE'}
+                        'Реал тайм \n ПЦР зонд': 'PCR_WITH_ZOND',
+                        'Реал тайм \n ПЦР sybr':'PCR_SYBRE'}
 
 keyboard_for_buttons = telebot.types.InlineKeyboardMarkup().add(
         telebot.types.InlineKeyboardButton([key for key in callback_query_handler][0],callback_data=[values for values in callback_query_handler.values()][0]),
