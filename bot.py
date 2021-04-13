a319b05a07f3d3ae361c933da05837038d8c1180
Anastasiia-Dakhnevich <77438911+Anastasiia-Dakhnevich@users.noreply.github.com>
2021-04-13 12:59:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ def chat (filename, message):
                   done = False
                   while done is False:
                            status, done = downloader.next_chunk()
-                           send(filename, message)
+                  send(filename, message)
          bot.send_message(message.chat.id, '\n\n Чтобы начать новый поиск, нажмите /protocols')
          
 
